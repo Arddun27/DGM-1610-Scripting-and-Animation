d29efd42a5e04257b41b5c3f4a520ba217827934
Spencer Richins <10841125@my.uvu.edu>
2020-11-18 20:30:19 -0700

    Started prototype 4, Sumo battle

---
+++ Sumo Battle/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 5;
+    private Rigidbody playerRB;
+    private GameObject focalPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float fInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalPoint.transform.forward * fInput * speed);
+    }
+}
