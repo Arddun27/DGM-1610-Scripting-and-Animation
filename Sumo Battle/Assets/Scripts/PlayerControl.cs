ef645a171e30ad8e1aeb79b25171297b52d5528e
Spencer Richins <10841125@my.uvu.edu>
2020-12-01 17:26:44 -0700

    4.3 complete, power up added, countdown timer stops power up effect after 7 seconds

--- Sumo Battle/Assets/Scripts/PlayerControl.cs
+++ Sumo Battle/Assets/Scripts/PlayerControl.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5;
+    public float speed = 5, powerUpStrength = 5;
     private Rigidbody playerRB;
     private GameObject focalPoint;
+    public bool hasPowerup = false;
+    public GameObject powerUpIndicator;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,39 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         float fInput = Input.GetAxis("Vertical");
+
         playerRB.AddForce(focalPoint.transform.forward * fInput * speed);
+
+        powerUpIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Power Up"))
+        {
+            Destroy(other.gameObject);
+            powerUpIndicator.gameObject.SetActive(true);
+            hasPowerup = true;
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerUpIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+    if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+    {
+        Rigidbody enemyRb = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+        enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        Debug.Log("Collided With: " + collision.gameObject.name + " with Power Up set to " + hasPowerup);
+    }
+
     }
 }
