d443fcd51cda067eeb873b67d00587b4aa7f50e7
Spencer Richins <10841125@my.uvu.edu>
2020-12-07 16:02:34 -0700

    Finished prototype 4, enemies and power ups spawn with waves using for and while loops.

--- Sumo Battle/Assets/Scripts/SpawnManager.cs
+++ Sumo Battle/Assets/Scripts/SpawnManager.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject enemyPrefab;
+    public GameObject enemyPrefab, powerupPrefab;
     private float spawnRange = 9;
+    public int enemyCount, waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+       SpawnEnemyWave(waveNumber);
+       Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for(int i = 0; i < enemiesToSpawn; i++)
+        {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
         
     }
 
